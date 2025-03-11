fbe3dd71edb4f2dc6e5caabdf5cf4806f35f98ca
Krystian Filipek <kf446@cam.ac.uk>
2025-03-11 23:58:01 +0000

    Error handling for image saver

--- src/image_saver.rs
+++ src/image_saver.rs
@@ -1,8 +1,12 @@
 use image::DynamicImage;
 use std::fs::File;
 use std::path::Path;
+use std::error::Error;
 
-pub fn save_image(img: &DynamicImage, path: &str) -> Result<(), Box<dyn std::error::Error>> {
-    img.save(path)?;
-    Ok(())
+pub fn save_image(img: &DynamicImage, path: &str) -> Result<(), Box<dyn Error>> {
+    let path = Path::new(path);
+    if let Some(parent) = path.parent() {
+        std::fs::create_dir_all(parent)?;
+    }
+    img.save(path).map_err(|e| Box::new(e) as Box<dyn Error>)
 }
